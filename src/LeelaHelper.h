68ea520626b60810ae6493983fc51cf4831e911f
George Guliashvili <guliashvili@users.noreply.github.com>
2020-03-22 13:28:03 +0000

    server ready responding with suggested move and node initial scratch

---
+++ src/LeelaHelper.h
@@ -0,0 +1,12 @@
+//
+// Created by George Guliashvili on 21/03/2020.
+//
+
+#ifndef LEELAZ_LEELA_H
+#define LEELAZ_LEELA_H
+
+#include <memory>
+#include "GameState.h"
+std::unique_ptr<GameState> init(int argc, char *argv[]);
+
+#endif //LEELAZ_LEELA_H
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -4,6 +4,7 @@
 
 // Added for the json-example
 #define BOOST_SPIRIT_THREADSAFE
+
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
@@ -12,6 +13,7 @@
 #include <boost/filesystem.hpp>
 #include <fstream>
 #include <vector>
+
 #ifdef HAVE_OPENSSL
 #include "crypto.hpp"
 #endif
@@ -19,55 +21,71 @@
 using namespace std;
 // Added for the json-example:
 using namespace boost::property_tree;
+#include   "Training.h"
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
+
+#include "config.h"
+
+#include <boost/program_options.hpp>
+#include <memory>
+#include <string>
+#include "LeelaHelper.h"
+#include "GTP.h"
+
+using namespace Utils;
 
 using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
 
-int main() {
-  // HTTP-server at port 8080 using 1 thread
-  // Unless you do more heavy non-threaded processing in the resources,
-  // 1 thread is usually faster than several threads
+int main(int argc, char *argv[]) {
   HttpServer server;
-  server.config.port = 8080;
+  server.config.port = 1999;
+  std::cerr<<"aq 0"<<std::endl;
+  server.resource["^/json$"]["POST"] = [argc, argv](shared_ptr<HttpServer::Response> response,
+                                                    shared_ptr<HttpServer::Request> request) {
+    std::cerr<<"aq 1"<<std::endl;
+      static auto game = init(argc, argv);
+      std::cerr<<"aq 3"<<std::endl;
+      Training::clear_training();
+      game->reset_game();
+      std::cerr<<"aq 4"<<std::endl;
 
-
-
-  // POST-example for the path /json, responds firstName+" "+lastName from the posted json
-  // Responds with an appropriate error message if the posted json is not valid, or if firstName or lastName is missing
-  // Example posted json:
-  // {
-  //   "firstName": "John",
-  //   "lastName": "Smith",
-  //   "age": 25
-  // }
-  server.resource["^/json$"]["POST"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
       try {
-        ptree pt;
-        read_json(request->content, pt);
+        auto j3 = json::parse(request->content);
+        for (const auto &move : j3.at("moves")) {
+          const auto x = move.at("x").get<int>();
+          const auto y = move.at("y").get<int>();
+          const auto isBlack = move.at("isBlack").get<int>();
 
-        auto name = pt.get<string>("firstName") + " " + pt.get<string>("lastName");
+          const auto vertex = game->board.get_vertex(x, y);
+          game->play_move((int) !isBlack, vertex);
+        }
+        const auto commandSpec = j3.at("commandSpec");
+        const auto command = commandSpec.at("command").get<string>();
+        if(true) // command ===
+        {
+          GTP::execute(*game, "genmove " +  (game->board.black_to_move() ? string("b") : string("w")));
+        }
+        const auto [x,y]  = game->board.get_xy(game->get_last_move());
+        const auto answer = json({"move", {{"x", x}, {"y", y}}}).dump();
 
-        *response << "HTTP/1.1 200 OK\r\n"
-                  << "Content-Length: " << name.length() << "\r\n\r\n"
-                  << name;
+        response->write(answer);
       }
-      catch(const exception &e) {
-        *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
-                  << e.what();
+      catch (const exception &e) {
+        response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
       }
-
-
-      // Alternatively, using a convenience function:
-      // try {
-      //     ptree pt;
-      //     read_json(request->content, pt);
-
-      //     auto name=pt.get<string>("firstName")+" "+pt.get<string>("lastName");
-      //     response->write(name);
-      // }
-      // catch(const exception &e) {
-      //     response->write(SimpleWeb::StatusCode::client_error_bad_request, e.what());
-      // }
   };
+  promise<unsigned short> server_port;
+  thread server_thread([&server, &server_port]() {
+      // Start server
+      server.start([&server_port](unsigned short port) {
+          server_port.set_value(port);
+      });
+  });
+  server_thread.join();
 
+  return 0;
 
 }
